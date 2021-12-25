15262f660efe9e04b32314a74d8edb73409cabd9
Fabian Canas <fcanas@gmail.com>
2021-12-24 21:06:03 -0500

    Release dit 0.0.2

--- Formula/dit.rb
+++ Formula/dit.rb
@@ -3,9 +3,9 @@
 class Dit < Formula
   desc "Basic CLI for Reminders on macOS"
   homepage "https://github.com/fcanas/dit"
-  url "https://github.com/fcanas/dit/archive/refs/tags/0.0.1.tar.gz"
+  url "https://github.com/fcanas/dit/archive/refs/tags/0.0.2.tar.gz"
   version "0.0.1"
-  sha256 "bf95244ffaeff5afc0adc0c7810490b98eeb27196b994e168393e5bcf1724b10"
+  sha256 "39477f86c77d7c47199828f9a622c740f261f1540ce0a0b9744ee454b07d6aa9"
   license "Apache-2.0"
 
   depends_on :xcode
