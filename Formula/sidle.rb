846230847435530f96c2aa643834d09a5cb700fc
Fabian Canas <fcanas@gmail.com>
2022-01-22 20:22:52 -0500

    Remove redundant version numbers

--- Formula/sidle.rb
+++ Formula/sidle.rb
@@ -2,7 +2,6 @@ class Sidle < Formula
   desc "Wordle CLI Assistant"
   homepage "https://github.com/fcanas/sidle"
   url "https://github.com/fcanas/sidle/archive/refs/tags/0.2.tar.gz"
-  version "0.2"
   sha256 "10d07e5dc39c0a5812eb8f30a7617235b2fe1365f83584aad43910817b0014d6"
   license "MIT"
 
