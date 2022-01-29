74e7eeaf12f6bc1631d5fab0a3d386daff7391d1
Fabian Canas <fcanas@gmail.com>
2022-01-29 15:38:16 -0500

    Reversion scrape to 0.2

--- Formula/scrape.rb
+++ Formula/scrape.rb
@@ -1,8 +1,8 @@
 class Scrape < Formula
   desc "Download HLS Streams"
   homepage "https://github.com/fcanas/scrape"
-  url "https://github.com/fcanas/scrape/archive/refs/tags/0.1.1.tar.gz"
-  sha256 "33a7a6bfe755da02cbe0121ee6cf048e506004c069ef730e280c029814741e27"
+  url "https://github.com/fcanas/scrape/archive/refs/tags/0.2.tar.gz"
+  sha256 "b3dff6008bfa0b8122da850a82b9995b261d6871b7e2e75fdffa85b8b18a18c0"
   license "MIT"
 
   depends_on :macos
